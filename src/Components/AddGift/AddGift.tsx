078e756767a2ce526c55692268c3137f3c0bb6a0
Mariusz <mario.m4@interia.pl>
2022-11-23 14:15:11 +0100

    Added sendForm function. We can send object to backend Added new state - resultInfo - We can see info about added item

--- src/Components/AddGift/AddGift.tsx
+++ src/Components/AddGift/AddGift.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import { CreateGiftReq } from "types";
+import React, {FormEvent, useState} from "react";
+import { CreateGiftReq, GiftEntity } from "types";
 import {Spinner} from "../common/Spinner/Spinner";
 
 export const AddGift = () => {
@@ -8,6 +8,7 @@ export const AddGift = () => {
         count: 0,
     })
     const [loading, setLoading] = useState<boolean>(false)
+    const [resultInfo, setResultInfo] = useState<string | null>(null)
 
     const updateForm = (key: string, value: any) => {
         setForm(form => ({
@@ -16,11 +17,38 @@ export const AddGift = () => {
         }))
     }
 
+    const sendForm = async (e: FormEvent) => {
+        e.preventDefault()
+
+        setLoading(true)
+
+        try {
+            const res = await fetch('http://localhost:3001/gift', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(form)
+            })
+            const data: GiftEntity = await res.json()
+            setResultInfo(`${data.name} added with ID ${data.id}`)
+        } finally {
+            setLoading(false)
+        }
+    }
+
     if (loading) {
         return <Spinner/>
     }
+
+    if (resultInfo !== null) {
+        return <div>
+            <p><strong>{resultInfo}</strong></p>
+            <button onClick={() => setResultInfo(null)}>Add another one</button>
+        </div>
+    }
     
-    return <form>
+    return <form onSubmit={sendForm}>
         <h2>Add Gift</h2>
         <p>
             <label>
@@ -40,6 +68,6 @@ export const AddGift = () => {
                     onChange={e => updateForm('count', Number(e.target.value))}/>
             </label>
         </p>
-        <button>Add</button>
+        <button type="submit">Add</button>
     </form>
 }
