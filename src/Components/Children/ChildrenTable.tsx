ec6dcc758d8dfe164c1488a68a00b33cc30790f4
Mariusz <mario.m4@interia.pl>
2022-12-02 18:16:52 +0100

    Added Child table

---
+++ src/Components/Children/ChildrenTable.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {ChildEntity, GiftEntity } from "types";
+import {ChildTableRow} from "./ChildTableRow";
+
+interface Props {
+    giftsList: GiftEntity[];
+    childrenList: ChildEntity[]
+}
+
+export const ChildrenTable = (props: Props) => (
+    <table>
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Gift</th>
+        </tr>
+        </thead>
+        <tbody>
+        {
+            props.childrenList.map(child => (
+                <ChildTableRow
+                    key={child.id}
+                    child={child}
+                    giftsList={props.giftsList}
+                />
+            ))
+        }
+        </tbody>
+    </table>
+)
