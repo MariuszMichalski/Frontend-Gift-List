8a95ddf3a8cf5e7c9aac3f35d5e64a837bd3fee3
Mariusz <mario.m4@interia.pl>
2022-12-02 18:27:35 +0100

    changed host number

--- src/views/SingleGiftView.tsx
+++ src/views/SingleGiftView.tsx
@@ -1,16 +1,16 @@
 import React, {useEffect, useState} from "react";
 import {GetSingleGiftRes} from "types";
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 
 export const SingleGiftView = () => {
-    const [giftInfo, setGiftInfo] = useState< GetSingleGiftRes | null>(null)
+    const [giftInfo, setGiftInfo] = useState<GetSingleGiftRes | null>(null)
     const {giftId} = useParams();
 
     useEffect(() => {
 
         (async () => {
 
-            const res = await fetch(`http://localhost:3000/gift/${giftId}`);
+            const res = await fetch(`http://localhost:3001/gift/${giftId}`);
             setGiftInfo(await res.json())
 
         })();
@@ -24,5 +24,8 @@ export const SingleGiftView = () => {
     return <>
         <h1>{giftInfo.gift.name}</h1>
         <p>This gift has ID {giftInfo.gift.id}. We had {giftInfo.gift.count} of this item and {giftInfo.givenCount} were already given</p>
+        <p>
+            <Link to="/gift">Back</Link>
+        </p>
     </>
 }
